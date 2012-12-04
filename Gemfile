b1da45d614e42ea445d4788edc3ce59cef843a45
Brian Dear <superacidjax@me.com>
2012-12-04 09:39:31 -0500

    Setup base layout

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.9'
+gem 'sqlite3'
+
+gem 'hashrocket-rails'
+gem 'jquery-rails'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development, :test do
+  gem 'thin'
+  gem 'pry-debugger'
+  gem 'pry-rails'
+  gem 'pry-stack_explorer'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Salsa::Application.config.secret_token = '69e7da814ae6cdfd9157437027a733534dac827c494f49e2c8eb8fa5cd57b3965994514551ea03f3a660f1277acbbd01f42e9e74e2469bc35c85469d3f329768'
