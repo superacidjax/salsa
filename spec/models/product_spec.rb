f4d3b4a1884d6f2525f1cf348f91360439194de3
Brian Dear <superacidjax@me.com>
2012-12-04 16:54:44 -0500

    adds product spec stub

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe Product do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @product = Product.new(title: "Blueberry Salsa", image_url: 'xzy', price: 3.99, description: "This is awesome sauce!")
+  end
+
+subject { @product }
+  it { should respond_to(:title) }
 end
