ecd5244a27c964fd71bb331e4dd047cfa75bc9b9
Brian Dear <superacidjax@me.com>
2012-12-05 09:34:38 -0500

    modifies some product validation - WIP

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
 
   mount_uploader :image_url, ProductImageUploader
+
+  validates :title, presence: true
+  validates :image_url, presence: true
+  validates :description, presence: true
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,10 +1,33 @@
 require 'spec_helper'
 
 describe Product do
+
   before do
-    @product = Product.new(title: "Blueberry Salsa", image_url: 'xzy', price: 3.99, description: "This is awesome sauce!")
+    @product = Product.new(title: "Blueberry Salsa", image_url: 'xzasfafafy',
+                           price: 3.99, description: "This is awesome sauce!")
   end
 
 subject { @product }
+
   it { should respond_to(:title) }
+  it { should respond_to(:image_url) }
+  it { should respond_to(:price) }
+  it { should respond_to(:description) }
+
+  it { should be_valid }
+
+  describe "when title is not present" do
+    before { @product.title = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when image_url is not present" do
+    before { @product.image_url = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when description is not present" do
+    before { @product.description = " " }
+    it { should_not be_valid }
+  end
 end
